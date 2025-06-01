0e5c072c408e1dfa7c308b22a19465bb3c502983
bioharz <bioharz3@gmail.com>
2025-06-01 04:05:51 +0200

    adapt deployment script

--- bl-evm/script/Token.s.sol
+++ bl-evm/script/Token.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Token} from "../src/Token.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract TokenScript is Script {
+    Token public token;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        token = new Token();
 
         vm.stopBroadcast();
     }
